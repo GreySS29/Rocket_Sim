36a9831ef2aab914d9009966b1274425f31c5330
GreySS29 <razor2911ss@gmail.com>
2026-07-05 17:31:33 +0900

    gravForce_vector

--- Rocket_sim_0.2v/include/Earth.h
+++ Rocket_sim_0.2v/include/Earth.h
@@ -4,13 +4,27 @@
 class Earth : public Physic_object
 {
     private:
-    const double radius = 6371000.0;
+    const double RADIUS_E = 6371000.0; //m
+    const double ATMOSPHERE_HEIGHT = 100000.0;//m 
+    const double AIR_DENSITY_SURFACE = 1.225; // kg/m3
 
     public:
     Earth () :Physic_object ({0,0,0}, 5.972E+24) {}; 
 
     Vector3D surfacePoint(double height_above_surface) const 
     {
-        return position + Vector3D{0,radius+height_above_surface,0};
+        return position + Vector3D{0,RADIUS_E+height_above_surface,0};
     };
+
+    double get_air_density(double height) const 
+    {
+        if (height >= ATMOSPHERE_HEIGHT) return 0; // approxim .. ?
+        return AIR_DENSITY_SURFACE * std::exp(-height/8500); //exp==pow
+    }
+
+    Vector3D get_dragForce_vec(const auto& object)
+    {
+        double density = getAirDensity(object.get_position().y);
+        if (density ==0) return Vector3D(0,0,0);
+    }
 };
--- Rocket_sim_0.2v/main.cpp
+++ Rocket_sim_0.2v/main.cpp
@@ -19,7 +19,9 @@ int main() {
     Engine engine {20000.0,100.0,earth.surfacePoint(2), 50.0};
     Tank tank {earth.surfacePoint(10), 50,20};
     std::unique_ptr stage = Stage::creat_stage(engine, tank, earth);
-    double s = gravity_magnitude (earth, *stage);
+    double s = gravity_magnitude (*stage,earth);
     std::cout << "grav: "<< s<< '\n';
+    Vector3D grav_vec = gravity_vec(*stage, earth);
+    std::cout << "grav_vec: "<< grav_vec<< '\n';
     return 0;
 } 
