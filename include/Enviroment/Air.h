eeb2102d0e1db266fb4c3d22e8179721a2b1c614
GreySS29 <razor2911ss@gmail.com>
2026-08-08 20:02:04 +0900

    start to expand Air's properties

---
+++ include/Enviroment/Air.h
@@ -0,0 +1,16 @@
+
+
+class Air {
+    private:
+    
+    double temperature;          // T, K
+    double pressure;             // p, Pa
+    double density;              // rho, kg/m^3
+    double speedOfSound;         // a, m/s
+
+
+
+
+
+    public:
+}
